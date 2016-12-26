1aafab150a28e2f60b5bfd67b49b1ac2579708b4
KShih <kontonsine@gmail.com>
2016-12-26 23:49:05 +0800

    Merge two commit

--- func.c
+++ func.c
@@ -3,6 +3,25 @@
 char *getStudentID(char *id)
 {
 	/* Return your student id. */
+<<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+	strcpy(id, "1032994");
+	
+>>>>>>> c43ec6b... Update func.c
+=======
+	
+	strcpy(id,"1032994");
+>>>>>>> 4a0d09a... Update func.c
+>>>>>>> 89c6cb3... Merge two commit
+=======
+	
+	strcpy(id,"1032994");
+>>>>>>> 4a0d09a... Update func.c
 	return id;
 }
